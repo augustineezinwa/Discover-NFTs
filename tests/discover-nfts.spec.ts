ca7f69ff40e6f2c7ffe5b9e66f949658caccb10c
Augustine Ezinwa <augustineezinwa@gmail.com>
2023-03-24 12:03:15 +0100

    fix failing test

--- tests/discover-nfts.spec.ts
+++ tests/discover-nfts.spec.ts
@@ -4,7 +4,7 @@ test('has title', async ({ page }) => {
   await page.goto('https://develop--look-up-nfts.netlify.app/');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/React App/);
+  await expect(page).toHaveTitle(/Discover-NFTs/);
 });
 
 
