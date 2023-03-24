3c71bbd1fb0619a78261f86be8ea4ae553a80d99
Augustine Ezinwa <augustineezinwa@gmail.com>
2023-03-24 11:59:40 +0100

    improve UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,29 @@
-import React, { useEffect, useState } from "react";
-import { ReactComponent as ReactLogo } from "./logo.svg";
-import { ReactComponent as IdleLogo } from './idle.svg';
+import React, { useState } from "react";
 import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
 import Container from "@mui/material/Container";
-import Divider from "@mui/material/Divider";
-import TextField from "@mui/material/TextField";
+
 import Grid from "@mui/material/Grid";
 import NftCard from "./NftCard";
 import NftModal from "./NftModal";
+import Typography from "@mui/material/Typography";
+import { createTheme, ThemeProvider } from "@mui/material";
+import Wishes from "./Wishes";
 
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import "./App.css";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: [
+      'Baloo Bhai',
+      'Roboto'
+    ].join(','),
+  },
+});
 
 const App = () => {
   const [walletAddress, setWalletAdress] = useState("");
@@ -25,88 +36,81 @@ const App = () => {
 
   const fetchNft = async (walletAddress: string) => {
     setError("");
-    try{
-    const response = await fetch(
-      `https://api.opensea.io/api/v1/assets?owner=${walletAddress}`
-    );
-    const data = await response.json();
-    setNftData(data.assets);
-    if(response.status === 400) setError(INVALID_WALLET_ADDRESS_ERROR);
-    } catch(e) {
+    try {
+      const response = await fetch(
+        `https://api.opensea.io/api/v1/assets?owner=${walletAddress}`
+      );
+      const data = await response.json();
+      setNftData(data.assets);
+      if (response.status === 400) setError(INVALID_WALLET_ADDRESS_ERROR);
+    } catch (e) {
       setError("Sorry, we lost internet connection to you, try reconnecting.");
     }
   };
-
-  useEffect(() => {
-    fetchNft(walletAddress);
-  }, [walletAddress]);
 
   const loadModal = (data: any) => {
     setIsModalOpen(true);
     setModalData(data);
   };
 
-  const hasError = error.length > 0 && !!walletAddress;
-
   return (
     <>
-      {isModalOpen && (
-        <NftModal
-          isOpen={isModalOpen}
-          onOpenModal={setIsModalOpen}
-          data={modalData}
-        />
-      )}
-      <Container maxWidth="xl">
-        <Box
-          sx={{
-            bgcolor: "#ffff",
-            minHeight: "100vh",
-            padding: 5,
-            mt: 5,
-            flexGrow: 1,
-            borderRadius: 5,
-          }}
-        >
-          <h1 className="header">
-            Discover
-            <ReactLogo />
-            NFTs
-          </h1>
-          <div>
-            <TextField
-              value={walletAddress}
-              fullWidth
-              label="# Enter wallet address"
-              id="walletAddress"
-              variant="outlined"
-              sx={{ mb: 2 }}
-              onChange={(e) => setWalletAdress(e.target.value)}
-              error={hasError}
-              helperText={hasError && error}
-            />
-          </div>
+      <Wishes width="50%" height="50%" className="bg" />
+      <ThemeProvider theme={theme}>
+        {isModalOpen && (
+          <NftModal
+            isOpen={isModalOpen}
+            onOpenModal={setIsModalOpen}
+            data={modalData}
+          />
+        )}
+        <Container maxWidth="xl">
+          <Box
+            sx={{
+              minHeight: "100vh",
+              padding: 5
+            }}
+          >
+            <Box
+              sx={{
+                minHeight: "100vh",
+                border: 'none',
+              }}>
+              <Typography variant="h2" gutterBottom pt={20} sx={{ zIndex: 30 }}>
+                Discover NFTs on OpenSea
+              </Typography>
 
-          <h3 className="search">Search Results: {nftData?.length || 0}</h3>
-          {!!nftData?.length && <Divider component="hr" sx={{ mb: 2 }} />}
-          {!nftData?.length && <div className="idle">
-            <IdleLogo />
-          </div>}
+              <form onSubmit={() => fetchNft(walletAddress)}>
+                <div className="walletBox">
+                  <label htmlFor="walletAddress" className="labelText">Enter Wallet Address:</label>
+                  <span>
+                    <input className="walletAddress" value={walletAddress} type="text" name="walletAddress" id="walletAddress" onChange={(e) => setWalletAdress(e.target.value)} />
+                    <Button className="searchButton" size="small" variant="contained" onClick={() => fetchNft(walletAddress)}>Search</Button>
+                  </span>
+                </div>
+              </form>
 
-          <Grid container spacing={2}>
-            {!!nftData?.length && nftData?.map((data, index) => (
-              <Grid key={index} item xs={12} sm={6} md={4} lg={3} xl={3}>
-                <NftCard
-                  title={data.name}
-                  imageDescription=""
-                  imageUrl={data.image_url}
-                  onOpenModal={(e) => loadModal(data)}
-                />
+              {!!nftData?.length && <h3 className="search">Search Results: {nftData?.length || 0}</h3>}
+              {(nftData?.length === 0 || error) && <div className="idle">
+                Search Not Found
+              </div>}
+
+              <Grid container spacing={2}>
+                {!!nftData?.length && nftData?.map((data, index) => (
+                  <Grid key={index} item xs={12} sm={6} md={4} lg={3} xl={3}>
+                    <NftCard
+                      title={data.name}
+                      imageDescription=""
+                      imageUrl={data.image_url}
+                      onOpenModal={(e) => loadModal(data)}
+                    />
+                  </Grid>
+                ))}
               </Grid>
-            ))}
-          </Grid>
-        </Box>
-      </Container>
+            </Box>
+          </Box>
+        </Container>
+      </ThemeProvider>
     </>
   );
 };
